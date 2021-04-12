18e5a4f2b236aa32d8b4b5f96069b99f14bea98d
seanlawless <seanklawless76@gmail.com>
2021-04-11 20:16:24 -0500

    Website Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,10 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>InAvision Media</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          InAvision Media is a videography company with a passion for telling
+          stories .
         </p>
       </div>
     </div>
@@ -26,7 +23,7 @@ const Header = props => (
               props.onOpenArticle('intro')
             }}
           >
-            Intro
+            Sean K. Lawless
           </button>
         </li>
         <li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,45 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import mypic from '../images/my-pic.jpg'
+import videographer from '../images/videographer.jpg'
+import emailjs from 'emailjs-com'
+
+import {
+  Field,
+  Label,
+  Control,
+  Input,
+  Button,
+  Icon,
+  Textarea,
+  Notification,
+} from 'rbx'
 
 class Main extends React.Component {
   render() {
+    function sendEmail(e) {
+      e.preventDefault()
+
+      emailjs
+        .sendForm(
+          'service_tj1yg2f',
+          'template_inavision',
+          e.target,
+          'user_MorFstgNrIZDbSjBDvtv9'
+        )
+        .then(
+          result => {
+            console.log(result.text)
+          },
+          error => {
+            console.log(error.text)
+          }
+        )
+      alert('We have received your message and will contact you back soon')
+      e.target.reset()
+    }
+
     let close = (
       <div
         className="close"
@@ -30,27 +66,23 @@ class Main extends React.Component {
         >
           <h2 className="major">Intro</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={mypic} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            Since graduating from college at UNLV 2004, I have been working as a
+            freelancer. My freelance experience began in 2011 when I got my
+            start filming weddings. Shortly thereafter, I decided to pursue my
+            interest in producing film and video. As a freelancer, I have found
+            my niche in editing from transitioning from Final Cut Pro to Adobe
+            Premiere. I have edited anything from promos, trailers, short films,
+            etc. I won my first award from the Houston World Film Fest back in
+            2019, one of the best days of my life. I am currently attending
+            NuCamp to learn and earn certification with web development to be
+            more versatile, but still have that passion for post production. I
+            have an ordinary life, family man with a beautiful wife and two
+            awesome kids. Every morning, I workout to start my day right. On the
+            weekends, I enjoy spending time with my family and eventually get
+            back to traveling.
           </p>
           {close}
         </article>
@@ -63,23 +95,53 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
-          <span className="image main">
+          {/* <span className="image main">
             <img src={pic02} alt="" />
-          </span>
+          </span> */}
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/M2lbPd2B42I"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/Jox7yyJwOGs"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/TEem5A8zWGA"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/FGGDuSvPUJM"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+            We specialize in unique and creative ways to show the world your
+            brand.We direct and produce beautiful cinematic short movies for
+            weddings, businesses or organizations. Reach your customers with
+            video that's designed specifically to support your business' goals.
           </p>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            The design of your logo or ad says a lot about your company...an
+            understatement. Let us create your logo, ad or social media posts
+            that will grab your customers' attention and show them you mean
+            business.
           </p>
           {close}
         </article>
@@ -93,16 +155,15 @@ class Main extends React.Component {
         >
           <h2 className="major">About</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={videographer} alt="" />
           </span>
+
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            InAvision Media is a production company specializing in visual media
+            that delivers impact while engaging its target audience. We know how
+            it goes when trying to create fascinating films on a small budget
+            without a big crew. InAvision Media has won an award for best crime
+            drama short film through Houston World Film Fest.
           </p>
           {close}
         </article>
@@ -115,7 +176,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form onSubmit={sendEmail}>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -130,13 +191,14 @@ class Main extends React.Component {
             </div>
             <ul className="actions">
               <li>
-                <input type="submit" value="Send Message" className="special" />
+                <input type="submit" value="Send Message" className="" />
               </li>
               <li>
                 <input type="reset" value="Reset" />
               </li>
             </ul>
           </form>
+
           <ul className="icons">
             <li>
               <a
